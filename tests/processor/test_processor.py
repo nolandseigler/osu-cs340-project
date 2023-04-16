d0d84c7a9b3496127abf6db011fde4699594a11d
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2023-04-16 17:37:12 -0400

    wip. was working on getting a row count based on files but that is no longer needed.

---
+++ tests/processor/test_processor.py
@@ -0,0 +1,9 @@
+from what_the_fec.client.bulk_client import Client as BulkClient, Config as BulkClientConfig
+from what_the_fec.processor.processor import Config, Processor
+
+# NOTE: test just used to run code not even close to any decent tests in here.
+def test_download_data():
+    client = BulkClient(config=BulkClientConfig())
+    processor = Processor(bulk_client=client, config=Config(min_cycle_year=2020))
+    # print(processor.process())
+    assert True
