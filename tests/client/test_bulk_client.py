085a2ed8a532e2710ee858c8965203b2824edca8
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2023-05-21 14:18:04 -0400

    remove test for removed code

--- tests/client/test_bulk_client.py
+++
@@ -1,7 +0,0 @@
-from what_the_fec.client.bulk_client import Client, Config, FileType
-
-
-def test_download_data():
-    client = Client(config=Config())
-    data = client.download_data(file_type=FileType.ALL_CANDIDATES, cycle_year=2022)
-    assert type(data["weball22.txt"]) == type(b"")
