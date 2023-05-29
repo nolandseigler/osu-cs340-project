9a0292f2521e791b49e0073ca4295545985fed01
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2023-05-28 21:10:14 -0400

    use structlog for logging, write access log and error log, and use gunicorn locally like in production.

---
+++ what_the_fec/logging.py
@@ -0,0 +1,68 @@
+import logging
+import sys
+
+import structlog
+
+# Citation for the following code:
+# Date: 05/28/2023
+# Copied from /OR/ Adapted from /OR/ Based on:
+# https://www.structlog.org/en/stable/standard-library.html#rendering-within-structlog
+
+# Just call this once at entrypoint
+def logging_init(log_level: str = "INFO"):
+    if not isinstance(log_level, str):
+        raise TypeError("invalid log level")
+    
+    log_level = logging.getLevelName(level=log_level)
+    if isinstance(log_level, str):
+        raise ValueError("invalid log level")
+    
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=log_level
+    )
+    structlog.configure(
+        processors=[
+            # If log level is too low, abort pipeline and throw away log entry.
+            structlog.stdlib.filter_by_level,
+            # Add the name of the logger to event dict.
+            structlog.stdlib.add_logger_name,
+            # Add log level to event dict.
+            structlog.stdlib.add_log_level,
+            # Perform %-style formatting.
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            # Add a timestamp in ISO 8601 format.
+            structlog.processors.TimeStamper(fmt="iso"),
+            # If the "stack_info" key in the event dict is true, remove it and
+            # render the current stack trace in the "stack" key.
+            structlog.processors.StackInfoRenderer(),
+            # If the "exc_info" key in the event dict is either true or a
+            # sys.exc_info() tuple, remove "exc_info" and render the exception
+            # with traceback into the "exception" key.
+            structlog.processors.format_exc_info,
+            # If some value is in bytes, decode it to a unicode str.
+            structlog.processors.UnicodeDecoder(),
+            # Add callsite parameters.
+            structlog.processors.CallsiteParameterAdder(
+                {
+                    structlog.processors.CallsiteParameter.FILENAME,
+                    structlog.processors.CallsiteParameter.FUNC_NAME,
+                    structlog.processors.CallsiteParameter.LINENO,
+                }
+            ),
+            # Render the final event dict as JSON.
+            structlog.processors.JSONRenderer()
+        ],
+        # `wrapper_class` is the bound logger that you get back from
+        # get_logger().
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+        # `logger_factory` is used to create wrapped loggers that are used for
+        # OUTPUT. This one returns a `logging.Logger`. The final value (a JSON
+        # string) from the final processor (`JSONRenderer`) will be passed to
+        # the method of the same name as that you've called on the bound logger.
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        # Effectively freeze configuration after creating the first bound
+        # logger.
+        cache_logger_on_first_use=True,
+    )
