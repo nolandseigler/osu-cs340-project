803c3e6594e3c03b5123b6673e4a9bddcd798a57
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2023-05-31 21:24:05 -0400

    party_types

--- what_the_fec/routes/party_types/routes.py
+++ what_the_fec/routes/party_types/routes.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Request
+from typing import Annotated
+from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
 
-from what_the_fec.routes.party_types.endpoint_funcs import TABLE_NAME, get_all_func
+from what_the_fec.routes.party_types.endpoint_funcs import TABLE_NAME, create_single_func, get_all_func
 
 router = APIRouter(
     prefix=f"/{TABLE_NAME}",
@@ -9,6 +10,7 @@ router = APIRouter(
     responses={404: {"description": f"{TABLE_NAME} not found"}},
 )
 
+STR_FORM_FIELD = Annotated[str, Form()]
 
 @router.get("/", response_class=HTMLResponse)
 def get_all(request: Request):
@@ -16,4 +18,16 @@ def get_all(request: Request):
         conn=next(request.db_conn),
         request=request,
         templates=request.templates,
+    )
+
+@router.post("/")
+def create_single(
+    request: Request,
+    code: STR_FORM_FIELD,
+    short_name: STR_FORM_FIELD,
+):
+    return create_single_func(
+        conn=next(request.db_conn),
+        code=code,
+        short_name=short_name,
     )
